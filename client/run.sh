a06ade6d7b3a0368da02bd56ec722e38cf03d0ae
danieluss <danieluss@pm.me>
2020-01-19 13:26:58 +0100

    small amendment of client/run.sh

--- client/run.sh
+++ client/run.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"
 
-java -jar net-search-engine-*.jar -Dserver.port=${1-8080} -DsearchServer.host=${2-127.0.0.1} -DsearchServer.port=${3-10101}
+java -jar ./target/net-search-engine-*.jar -Dserver.port=${1-8080} -DsearchServer.host=${2-127.0.0.1} -DsearchServer.port=${3-10101}
